6d2bbd6834fc9bb9e09ffc9a81a36f10113bdc70
Vladyslav Holiachenko <v.holiachenko@steel.mil.ua>
2025-10-01 11:58:06 +0300

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from '@src/app.module';
 import { VersioningType } from '@nestjs/common';
@@ -22,21 +21,7 @@ async function bootstrap() {
 
     const configService = app.get<ConfigService>(ConfigService);
 
-    const rmq = configService.getOrThrow('rmq');
     const { port } = configService.getOrThrow('app');
-
-    app.connectMicroservice<MicroserviceOptions>({
-        transport: Transport.RMQ,
-        options: {
-            urls: [`amqp://${rmq.user}:${rmq.password}@${rmq.host}:${rmq.port}`],
-            queue: rmq.converterQueue,
-            queueOptions: {
-                durable: true,
-            },
-            noAck: false,
-            prefetchCount: 1,
-        },
-    });
 
     await app.startAllMicroservices();
     await app.listen(port);
--- src/modules/converter/converter.controller.ts
+++ src/modules/converter/converter.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Logger, Post } from '@nestjs/common';
-import { RmqContext, Ctx, EventPattern, Payload } from '@nestjs/microservices';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { RmqEvent } from '@src/modules/rmqClient/RmqEvent';
 import { ConverterService } from './converter.service';
 
 @Controller()
@@ -10,30 +8,14 @@ export class ConverterController {
 
     constructor(private readonly converterService: ConverterService) {}
 
-    @EventPattern(RmqEvent.convertImage)
-    async convertImage(@Payload() data: any, @Ctx() context: RmqContext) {
-        this.logger.log(`Handle ${RmqEvent.convertImage} event with data:`, data);
-
-        const dataObj = JSON.parse(data);
-        const isSuccess = await this.converterService.processFile(dataObj.s3key);
-
-        if (isSuccess) {
-            this.logger.log('Event successfully processed');
-        } else {
-            this.logger.warn('Unable to process event');
-        }
-
-        context.getChannelRef().ack(context.getMessage());
-    }
-
     @Cron(CronExpression.EVERY_HOUR)
     async pullImagesToConvert() {
-        await this.converterService.getFilesListFromMinio();
+        await this.converterService.convertHeicToJpg();
     }
 
     @Post('/start-convert')
-    async getCoursesList() {
-        await this.converterService.getFilesListFromMinio();
+    async getFilesListFromMinio() {
+        await this.converterService.convertHeicToJpg();
         return { success: true };
     }
 }
